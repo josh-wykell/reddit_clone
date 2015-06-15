a0ac598f02eb4093525a391af6e6a38cd409ab86
Michael E. Nash <utumno86@gmail.com>
2015-06-15 10:43:45 -0400

    some clean up

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -12,10 +12,21 @@ class CommentsController < ApplicationController
       render new, notice: "Comment failed"
     end
   end
+  
+  def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    redirect_to @link, notice: "Comment deleted"
+  end
 
   private
 
   def comment_params
     params.require(:comment).permit(:body)
+  end
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_link
+    @link = Link.find(params[:id])
   end
 end
--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,64 +1,42 @@
 class LinksController < ApplicationController
   before_action :set_link, only: [:show, :edit, :update, :destroy]
   before_filter :authenticate_user!, except: [:index, :show]
-  # GET /links
-  # GET /links.json
+  
   def index
     @links = Link.order("cached_votes_up DESC").page(params[:page])
   end
-
-  # GET /links/1
-  # GET /links/1.json
+  
   def show
   end
-
-  # GET /links/new
+  
   def new
     @link = current_user.links.build
   end
-
-  # GET /links/1/edit
+  
   def edit
   end
-
-  # POST /links
-  # POST /links.json
+  
   def create
     @link = current_user.links.build(link_params)
-
-    respond_to do |format|
-      if @link.save
-        format.html { redirect_to @link, notice: 'Link was successfully created.' }
-        format.json { render :show, status: :created, location: @link }
-      else
-        format.html { render :new }
-        format.json { render json: @link.errors, status: :unprocessable_entity }
-      end
+    
+    if @link.save
+      redirect_to @link, notice: 'Link was successfully created.'
+    else
+      render :new 
     end
   end
-
-  # PATCH/PUT /links/1
-  # PATCH/PUT /links/1.json
+  
   def update
-    respond_to do |format|
-      if @link.update(link_params)
-        format.html { redirect_to @link, notice: 'Link was successfully updated.' }
-        format.json { render :show, status: :ok, location: @link }
-      else
-        format.html { render :edit }
-        format.json { render json: @link.errors, status: :unprocessable_entity }
-      end
+    if @link.update(link_params) 
+      redirect_to @link, notice: 'Link was successfully updated.'
+    else
+      render :edit
     end
   end
-
-  # DELETE /links/1
-  # DELETE /links/1.json
+  
   def destroy
     @link.destroy
-    respond_to do |format|
-      format.html { redirect_to links_url, notice: 'Link was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to links_url, notice: 'Link was successfully destroyed.'
   end
   
   def upvote
