b9550137054ceff3889502d887b25b3583aefb5f
Bennett Chidoro <Chidoro9@yahoo.com>
2015-07-03 14:36:49 -0400

    added tags and did a little styling

--- app/models/link.rb
+++ app/models/link.rb
@@ -2,4 +2,17 @@ class Link < ActiveRecord::Base
   belongs_to :user
   has_many :comments
   acts_as_votable
+
+  has_many :taggings
+  has_many :tags, through: :taggings
+
+  def all_tags=(names)
+    self.tags = names.split(",").map do |name|
+      Tag.where(name: name.strip).first_or_create!
+    end
+  end
+
+  def all_tags
+    self.tags.map(&:name).join(", ")
+  end
 end
